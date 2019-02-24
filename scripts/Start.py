7ca94bfc681b254130dfbc4c5e2bc996f45106e5
Marius Göcke <marius.goecke@gmail.com>
2019-02-24 20:04:23 +0100

    improved readability of logs

--- scripts/Start.py
+++ scripts/Start.py
@@ -3,12 +3,21 @@ import pe
 import me
 import ce
 
+print("------------------------------------------------------------")
 print("Start generating evidences")
 ge
+print("Finished generating evidences")
+print("------------------------------------------------------------")
 print("Start preparing evidences")
 pe
+print("Finished preparing evidences")
+print("------------------------------------------------------------")
 print("Start merging evidences")
 me
+print("Finished merging evidences")
+print("------------------------------------------------------------")
 print("Start finding characteristical evidences")
 ce
+print("Finished finding characteristical evidences")
+print("------------------------------------------------------------")
 print("Finished")
